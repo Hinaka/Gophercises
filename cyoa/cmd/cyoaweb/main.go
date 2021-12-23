c81dc6170bb9ee44826a41d5661948257533a9e6
Hinaka <76215941+Hinaka@users.noreply.github.com>
2021-12-23 22:10:05 +0700

    building the http.Handler

--- cyoa/cmd/cyoaweb/main.go
+++ cyoa/cmd/cyoaweb/main.go
@@ -4,10 +4,13 @@ import (
 	"flag"
 	"fmt"
 	"github.com/hinaka/gophercises/cyoa"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "the port to start the CYOA web application on")
 	filename := flag.String("file", "gopher.json", "the JSON filename with the CYOA story")
 	flag.Parse()
 	fmt.Printf("Using the story in %s.\n", *filename)
@@ -18,5 +21,11 @@ func main() {
 	}
 
 	story, err := cyoa.JsonStory(f)
-	fmt.Printf("%+v\n", story)
+	if err != nil {
+		panic(err)
+	}
+
+	h := cyoa.NewHandler(story)
+	fmt.Printf("Starting the server at: %d\n\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
--- cyoa/story.go
+++ cyoa/story.go
@@ -2,8 +2,51 @@ package cyoa
 
 import (
 	"encoding/json"
+	"html/template"
 	"io"
+	"net/http"
 )
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTmpl))
+}
+
+var defaultHandlerTmpl = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Choose Your Own Story</title>
+</head>
+<body>
+<h1>{{.Title}}</h1>
+{{range .Paragraphs}}
+<p>{{.}}</p>
+{{end}}
+<ul>
+    {{range .Options}}
+    <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+    {{end}}
+</ul>
+</body>
+</html>`
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	if err != nil {
+		panic(err)
+	}
+}
 
 func JsonStory(r io.Reader) (Story, error) {
 	d := json.NewDecoder(r)
