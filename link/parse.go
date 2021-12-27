868a3674f02621cbe7cfbce6a64eef3078a5cd67
Hinaka <76215941+Hinaka@users.noreply.github.com>
2021-12-27 21:39:29 +0700

    dfs to find link nodes

--- link/parse.go
+++ link/parse.go
@@ -1,6 +1,10 @@
 package link
 
-import "io"
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"io"
+)
 
 // Link represent a link (<a href="...">) in an HTML document.
 type Link struct {
@@ -10,5 +14,24 @@ type Link struct {
 
 // Parse will take and HTML document and will return a slice of links parsed from it
 func Parse(r io.Reader) ([]Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+	nodes := linkNodes(doc)
+	for _, node := range nodes {
+		fmt.Println(node)
+	}
 	return nil, nil
+}
+
+func linkNodes(n *html.Node) []*html.Node {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		return []*html.Node{n}
+	}
+	var ret []*html.Node
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		ret = append(ret, linkNodes(c)...)
+	}
+	return ret
 }
