67f6d34cefca510d4e843c1b2367e0e83596adc3
Hinaka <76215941+Hinaka@users.noreply.github.com>
2022-01-01 16:17:13 +0700

    get links from html

--- sitemap/main.go
+++ sitemap/main.go
@@ -2,19 +2,41 @@ package main
 
 import (
 	"flag"
-	"io"
+	"fmt"
+	"github.com/hinaka/gophercises/link"
 	"net/http"
-	"os"
+	"net/url"
+	"strings"
 )
 
 func main() {
 	urlFlag := flag.String("url", "https://gophercises.com/", "the url you want to build a sitemap for")
 	flag.Parse()
 
-	resp, err:= http.Get(*urlFlag)
+	resp, err := http.Get(*urlFlag)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
-	io.Copy(os.Stdout, resp.Body)
+
+	links, _ := link.Parse(resp.Body)
+	reqUrl := resp.Request.URL
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+	base := baseUrl.String()
+
+	var hrefs []string
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			hrefs = append(hrefs, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			hrefs = append(hrefs, l.Href)
+		}
+	}
+	for _, href := range hrefs {
+		fmt.Println(href)
+	}
 }
