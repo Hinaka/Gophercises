247ff922b3a86f98c0dab9c18195cbdf2d482ee0
Hinaka <76215941+Hinaka@users.noreply.github.com>
2022-01-06 18:47:52 +0700

    create basic root command

--- task/cmd/root.go
+++ task/cmd/root.go
@@ -1,3 +1,8 @@
 package cmd
 
-var RootCmd =  
+import "github.com/spf13/cobra"
+
+var RootCmd = &cobra.Command{
+	Use:   "task",
+	Short: "Task is a CLI task manager",
+}
--- task/main.go
+++ task/main.go
@@ -1,5 +1,7 @@
 package main
 
-func main() {
+import "github.com/hinaka/gophercises/task/cmd"
 
+func main() {
+	cmd.RootCmd.Execute()
 }
