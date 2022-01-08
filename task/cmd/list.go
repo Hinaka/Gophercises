49531c10f8e56097c1e63a593d80495f945e867c
Hinaka <76215941+Hinaka@users.noreply.github.com>
2022-01-08 15:08:08 +0700

    finalize task list command

--- task/cmd/list.go
+++ task/cmd/list.go
@@ -1,11 +1,9 @@
-/*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
-*/
 package cmd
 
 import (
 	"fmt"
+	"github.com/hinaka/gophercises/task/db"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -15,7 +13,19 @@ var listCmd = &cobra.Command{
 	Use:   "list",
 	Short: "Lists all of your tasks.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("list called")
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("Something when wrong: ", err.Error())
+			os.Exit(1)
+		}
+		if len(tasks) == 0 {
+			fmt.Println("You have no tasks to do!")
+			return
+		}
+		fmt.Println("You have the following tasks:")
+		for i, task := range tasks {
+			fmt.Printf("%d. %s\n", i+1, task.Value)
+		}
 	},
 }
 
