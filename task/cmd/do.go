29ce7a14aeadf9fa7fa70888a2adbba16dc54978
Hinaka <76215941+Hinaka@users.noreply.github.com>
2022-01-08 15:48:04 +0700

    finalize task do command

--- task/cmd/do.go
+++ task/cmd/do.go
@@ -6,8 +6,9 @@ package cmd
 
 import (
 	"fmt"
-
+	"github.com/hinaka/gophercises/task/db"
 	"github.com/spf13/cobra"
+	"strconv"
 )
 
 // doCmd represents the do command
@@ -15,20 +16,36 @@ var doCmd = &cobra.Command{
 	Use:   "do",
 	Short: "Marks a task as complete.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("do called")
+		var ids []int
+		for _, arg := range args {
+			id, err := strconv.Atoi(arg)
+			if err != nil {
+				fmt.Println("Failed to parse the argument:", arg)
+			} else {
+				ids = append(ids, id)
+			}
+		}
+		tasks, err := db.AllTasks()
+		if err != nil {
+			fmt.Println("Something went wrong", err.Error())
+			return
+		}
+		for _, id := range ids {
+			if id <= 0 || id > len(tasks) {
+				fmt.Println("Invalid task number:", id)
+				continue
+			}
+			task := tasks[id-2]
+			err := db.DeleteTask(task.Key)
+			if err != nil {
+				fmt.Printf("Failed to mark \"%d\n as complete. Error: %s\n", id, err)
+			} else {
+				fmt.Printf("Marked \"%d\n as complete.\n", id)
+			}
+		}
 	},
 }
 
 func init() {
 	RootCmd.AddCommand(doCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// doCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// doCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
