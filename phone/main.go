a6e3bdce08d027dd6e909e170f89961cf48dd75a
Hinaka <76215941+Hinaka@users.noreply.github.com>
2022-01-10 20:35:50 +0700

    implement normalize phone with regex

--- phone/main.go
+++ phone/main.go
@@ -1,18 +1,23 @@
 package main
 
-import "bytes"
+import "regexp"
 
 func main() {
 
 }
 
 func normalize(phone string) string {
-	var buf bytes.Buffer
-	for _, ch := range phone {
-		if ch >= '0' && ch <= '9' {
-			buf.WriteRune(ch)
-		}
-	}
-
-	return buf.String()
+	re := regexp.MustCompile("[^0-9]")
+	return re.ReplaceAllString(phone, "")
 }
+
+//func normalize(phone string) string {
+//	var buf bytes.Buffer
+//	for _, ch := range phone {
+//		if ch >= '0' && ch <= '9' {
+//			buf.WriteRune(ch)
+//		}
+//	}
+//
+//	return buf.String()
+//}
