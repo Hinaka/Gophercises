0b9ed1f82f7b1496bf3625b411470896f7ad842e
Hinaka <76215941+Hinaka@users.noreply.github.com>
2022-01-10 20:26:26 +0700

    implement normalize phone with buffer

--- phone/main.go
+++ phone/main.go
@@ -1,9 +1,18 @@
 package main
 
+import "bytes"
+
 func main() {
 
 }
 
 func normalize(phone string) string {
-	return ""
+	var buf bytes.Buffer
+	for _, ch := range phone {
+		if ch >= '0' && ch <= '9' {
+			buf.WriteRune(ch)
+		}
+	}
+
+	return buf.String()
 }
