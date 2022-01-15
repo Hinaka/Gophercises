9173f43fa29a9767b98438affd2f984ecb2e749f
Hinaka <76215941+Hinaka@users.noreply.github.com>
2022-01-15 16:46:42 +0700

    refactor and finalize code

--- phone/main.go
+++ phone/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
+	phonedb "github.com/hinaka/gophercises/phone/db"
 	"regexp"
 )
 import _ "github.com/lib/pq"
@@ -15,42 +15,37 @@ const (
 )
 
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s sslmode=disable", host, port, user)
-	db, err := sql.Open("postgres", psqlInfo)
-	must(err)
-	must(resetDB(db, dbname))
-	db.Close()
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=disable", host, port, user)
+	must(phonedb.Reset("postgres", psqlInfo, dbname))
 
 	psqlInfo = fmt.Sprintf("%s dbname=%s", psqlInfo, dbname)
-	db, err = sql.Open("postgres", psqlInfo)
+	must(phonedb.Migrate("postgres", psqlInfo))
+
+	db, err := phonedb.Open("postgres", psqlInfo)
 	must(err)
 	defer db.Close()
 
-	must(createPhoneNumbersTable(db))
-	_, err = insertPhone(db, "1234567890")
-	must(err)
-	_, err = insertPhone(db, "123 456 7891")
-	must(err)
-	_, err = insertPhone(db, "(123) 456 7892")
-	must(err)
-	_, err = insertPhone(db, "(123) 456-7893")
-	must(err)
-	_, err = insertPhone(db, "123-456-7894")
-	must(err)
-	_, err = insertPhone(db, "123-456-7890")
-	must(err)
-	_, err = insertPhone(db, "1234567892")
-	must(err)
-	id, err := insertPhone(db, "(123)456-7892")
+	err = db.Seed()
 	must(err)
 
-	number, err := getPhone(db, id)
+	phones, err := db.AllPhones()
 	must(err)
-	fmt.Println("Number is...", number)
-
-	phones, err := allPhones(db)
 	for _, p := range phones {
-		fmt.Printf("%+v\n", p)
+		fmt.Printf("Working on... %+v\n", p)
+		number := normalize(p.Number)
+		if number != p.Number {
+			fmt.Println("Updating or removing...", number)
+			existing, err := db.FindPhone(number)
+			must(err)
+			if existing != nil {
+				must(db.DeletePhone(p.ID))
+			} else {
+				p.Number = number
+				must(db.UpdatePhone(&p))
+			}
+		} else {
+			fmt.Println("No changes required")
+		}
 	}
 }
 
@@ -58,77 +53,6 @@ func must(err error) {
 	if err != nil {
 		panic(err)
 	}
-}
-
-func resetDB(db *sql.DB, name string) error {
-	_, err := db.Exec("DROP DATABASE IF EXISTS " + name)
-	if err != nil {
-		return err
-	}
-	return createDB(db, name)
-}
-
-func createDB(db *sql.DB, name string) error {
-	_, err := db.Exec("CREATE DATABASE " + name)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func getPhone(db *sql.DB, id int) (string, error) {
-	var number string
-	err := db.QueryRow("SELECT value FROM phone_numbers WHERE id=$1", id).Scan(&number)
-	if err != nil {
-		return "", err
-	}
-	return number, nil
-}
-
-type phone struct {
-	id     int
-	number string
-}
-
-func allPhones(db *sql.DB) ([]phone, error) {
-	rows, err := db.Query("SELECT id, value FROM phone_numbers")
-	if err != nil {
-		return nil, err
-	}
-	defer rows.Close()
-
-	var ret []phone
-	for rows.Next() {
-		var p phone
-		if err := rows.Scan(&p.id, &p.number); err != nil {
-			return nil, err
-		}
-		ret = append(ret, p)
-	}
-	if err := rows.Err(); err != nil {
-		return nil, err
-	}
-	return ret, nil
-}
-
-func insertPhone(db *sql.DB, phone string) (int, error) {
-	statement := `INSERT INTO phone_numbers(value) VALUES($1) RETURNING id`
-	var id int
-	err := db.QueryRow(statement, phone).Scan(&id)
-	if err != nil {
-		return -1, err
-	}
-	return id, nil
-}
-
-func createPhoneNumbersTable(db *sql.DB) error {
-	statement := `
-		CREATE TABLE IF NOT EXISTS phone_numbers (
-			id SERIAL,
-			value VARCHAR(255)
-		)`
-	_, err := db.Exec(statement)
-	return err
 }
 
 func normalize(phone string) string {
